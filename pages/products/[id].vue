8cf929dd69378652c4e0628fcb91234650004e1d
Pratik Chakraborty <pratik@talkinglands.com>
2023-12-19 15:44:38 +0530

    twitter meta tags added

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,10 +1,15 @@
 <template>
     <Head>
         <Title>Nuxt Dojo | {{ product.title }}</Title>
+        <Meta name="description" :content="product.description" />
         <Meta name="og:title" :content="product.title" />
         <Meta name="og:image"
             :content="product.image" />
         <Meta name="og:description" :content="product.description" />
+        <Meta name="twitter:title" :content="product.title" />
+        <Meta name="twitter:image"
+            :content="product.image" />
+        <Meta name="twitter:description" :content="product.description" />
     </Head>
     <div>
         <h1>Single Product</h1>
@@ -15,15 +20,24 @@
 </template>
 
 <script setup>
+import axios from "axios";
 const { id } = useRoute().params;
 const uri = 'https://fakestoreapi.com/products/' + id
 
-// fetch the product
-const { data: product } = await useFetch(uri, { key: id })
+// Fetch the product using Axios
+let product = null;
 
-if (!product.value) {
-    throw createError({ statusCode: 404, statusMessage: 'Product Not Found', falat: true })
+try {
+    const response = await axios.get(uri);
+    product = response.data;
+} catch (error) {
+    throw createError({ statusCode: 404, statusMessage: 'Product Not Found', fatal: true });
 }
+
+if (!product) {
+    throw createError({ statusCode: 404, statusMessage: 'Product Not Found', fatal: true });
+}
+
 
 definePageMeta({
     layout: 'products'
